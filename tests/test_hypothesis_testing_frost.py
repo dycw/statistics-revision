1eb0379e9465289ddd66c8c91043fa3ab079605c
Derek Wan <d.wan@icloud.com>
2020-10-22 16:30:15 +0800

    Use isclose()

--- tests/test_hypothesis_testing_frost.py
+++ tests/test_hypothesis_testing_frost.py
@@ -7,6 +7,7 @@ from pandas import read_csv
 from scipy.stats import sem
 from scipy.stats import t
 from scipy.stats import ttest_1samp
+from scipy.stats import ttest_ind
 from statsmodels.stats.weightstats import DescrStatsW
 
 from statistics_revision import CODE_ROOT
@@ -20,25 +21,35 @@ def test_descriptive_statistics_fuel_cost_p28() -> None:
     df = read_csv(path)
     X = df["Fuel Cost"]
     assert len(X) == 25
-    assert mean(X) == 330.56
-    assert sem(X) == 30.8355357771949
-    assert std(X) == 151.06265719892525
+    assert isclose(mean(X), 330.6, atol=1e-1)
+    assert isclose(sem(X), 30.8, atol=1e-1)
+    assert isclose(std(X, ddof=1), 154.2, atol=1e-1)
 
 
-def test_1_sample_t_tests_p47() -> None:
+def test_1_sample_t_test_example_p47() -> None:
     path = BOOK_ROOT.joinpath("AssessmentScores.csv")
     df = read_csv(path)
     X = df["Score"]
     res = ttest_1samp(X, 60)
-    assert res.statistic == 1.41838979994091
-    assert res.pvalue == 0.17795265748623812
+    assert isclose(res.statistic, 1.42, atol=1e-2)
+    assert isclose(res.pvalue, 0.178, atol=1e-3)
     assert (n := len(X)) == 15
-    assert (mu := mean(X)) == 64.15818651056485
-    assert (sem_ := sem(X)) == 2.9316246568736433
+    assert isclose(mu := mean(X), 64.16, atol=1e-2)
+    assert isclose(sem_ := sem(X), 2.93, atol=1e-2)
     confidence = 0.95
     width = sem_ * t.ppf((1.0 + confidence) / 2, n - 1)
     ci_1 = mu - width, mu + width
     ci_2 = t.interval(confidence, n - 1, loc=mu, scale=sem_)
     ci_3 = DescrStatsW(X).tconfint_mean()
-    assert ci_1 == ci_2
-    assert isclose(ci_1, ci_3).all()
+    for ci in (ci_1, ci_2, ci_3):
+        assert isclose(ci, (57.87, 70.45), atol=1e-2).all()
+
+
+def test_2_sample_t_test_example_p50() -> None:
+    path = BOOK_ROOT.joinpath("t-TestExamples.csv")
+    df = read_csv(path)
+    X = df["Method A"]
+    Y = df["Method B"]
+    res = ttest_ind(X, Y)
+    assert isclose(res.statistic, -4.08, atol=1e-2)
+    assert isclose(res.pvalue, 0.0, atol=1e-3)
