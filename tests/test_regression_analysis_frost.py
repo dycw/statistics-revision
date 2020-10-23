c8b161bd52e9104e575d86760b60eca4cfcdcdb1
Derek Wan <d.wan@icloud.com>
2020-10-23 09:59:24 +0800

    Check confidence intervals

--- tests/test_regression_analysis_frost.py
+++ tests/test_regression_analysis_frost.py
@@ -31,9 +31,10 @@ def test_regression_model_plot_p43() -> None:
     model = OLS(Y, X).fit()
     X = df["Height M"]
     scatter = Scatter((X, Y)).opts(size=10)
+    params = model.params
     slope = Slope(
-        slope=model.params.loc["Height M"],
-        y_intercept=model.params.loc["const"],
+        slope=params.loc["Height M"],
+        y_intercept=params.loc["const"],
     ).opts(color="orange")
     plot = scatter * slope
     assert isinstance(plot, Overlay)
@@ -45,11 +46,20 @@ def test_regression_model_values_p52() -> None:
     X, Y = df[["const", "Height M"]], df["Weight kg"]
     model = OLS(Y, X).fit()
     assert isinstance(model.summary(), Summary)
-    assert isclose(model.params.loc["const"], -114.326, atol=1e-3)
-    assert isclose(model.bse.loc["const"], 17.4425, atol=1e-4)
-    assert isclose(model.tvalues.loc["const"], -6.554444, atol=1e-5)
-    assert isclose(model.pvalues.loc["const"], 0.0, atol=1e-3)
-    assert isclose(model.params.loc["Height M"], 106.505, atol=1e-3)
-    assert isclose(model.bse.loc["Height M"], 11.5500, atol=1e-4)
-    assert isclose(model.tvalues.loc["Height M"], 9.221177, atol=1e-5)
-    assert isclose(model.pvalues.loc["Height M"], 0.0, atol=1e-3)
+    params = model.params
+    bse = model.bse
+    tvalues = model.tvalues
+    pvalues = model.pvalues
+    assert isclose(params.loc["const"], -114.326, atol=1e-3)
+    assert isclose(bse.loc["const"], 17.4425, atol=1e-4)
+    assert isclose(tvalues.loc["const"], -6.554444, atol=1e-5)
+    assert isclose(pvalues.loc["const"], 0.0, atol=1e-3)
+    assert isclose(params.loc["Height M"], 106.505, atol=1e-3)
+    assert isclose(bse.loc["Height M"], 11.5500, atol=1e-4)
+    assert isclose(tvalues.loc["Height M"], 9.221177, atol=1e-5)
+    assert isclose(pvalues.loc["Height M"], 0.0, atol=1e-3)
+    ci = model.conf_int()
+    assert isclose(ci.loc["const", 0], -149.0, atol=1e-1)
+    assert isclose(ci.loc["const", 1], -79.7, atol=1e-1)
+    assert isclose(ci.loc["Height M", 0], 83.5, atol=1e-1)
+    assert isclose(ci.loc["Height M", 1], 129.5, atol=1e-1)
